6afc4bd9c2f56c01988a89a1810ee5fbb6ebe7f5
JackPoulis <jackpoulis@gmail.com>
2023-11-02 23:26:18 +0200

    Discard non unique solutions after each solution is found

--- queens_puzzle.c
+++ queens_puzzle.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-int main()
-{	
+int** generate_isometry_group(int a[]){
+    int i;
+    int** c = (int**) malloc(8*sizeof(int*));
+    for (i = 0; i < 8; i++){
+        c[i] = (int*) malloc(8*sizeof(int));
+    }
+    for (i = 0; i < 8; i++){
+        //Generate all possible versions of the solution "a" with rotation and translation
+        c[0][i] = a[i];
+        c[1][7 - a[i]] = i;
+        c[2][7 - i] = 7 - a[i];
+        c[3][a[i]] = 7 - i;
+        c[4][7 - i] = a[i];
+        c[5][i] = 7 - a[i];
+        c[6][7 - a[i]] = 7 - i;
+        c[7][a[i]] = i;
+    }
+    return c;
+}
+
+bool identical(int a[], int b[]){
+    int i;
+    bool identical = true;
+    for (i = 0; i < 8; i++){
+        //If one position is different then the
+        //two arrays are different
+        if (a[i] != b[i]){
+            identical = false;
+            break;
+        }
+    }
+    return identical;
+}
+
+int main(){	
 	//Variables initialization
-	int i, j, A[8], C[8][8], col, row, flag, end, n_solution, k, l, m, mirrored;
+	int i, j, A[8], col, row, n_solution, k, l, m;
+    bool threat, end;
 
     int** B = (int**) malloc(sizeof(int*));
 
@@ -15,115 +51,77 @@ int main()
 	n_solution = 0;
 	col = 0;
 	row = 0;
-	end = 0;
+	end = false;
 
 	//Find all solutions
-	do
-	{
-		flag = 1;
-
+	do{
         //If row is 0 column is 8 then a solution is found
-		if ((row == 0) && (col == 8))
-		{      
-            B[n_solution] = (int*) malloc(8 * sizeof(int));
-			for (i = 0; i < 8; i++)
-			{
-				B[n_solution][i] = A[i];
-			}
-			n_solution++;
-            B = (int**)realloc(B, (n_solution+1) * sizeof(int*));
+		if ((row == 0) && (col == 8)){ 
 
-			col = col - 2;
-			row = A[col] + 1;
-			A[7] = 16;
-			A[6] = 16;
+            //Check if the solution is unique****
+            int** iso_g = generate_isometry_group(A);
+            bool discard = false;
+            for (i=0;i<n_solution;i++){ //For each previous solutions
+                for (j=0;j<8;j++){ //For each of the current isometry solutions
+                    if (identical(B[i],iso_g[j])){
+                        discard = true;
+                        break;
+                    }
+                }
+                if(discard == true)break;
+            }
+
+            if(discard == false){
+                B = (int**)realloc(B, (n_solution+1) * sizeof(int*));
+                B[n_solution] = (int*) malloc(8 * sizeof(int));
+                for (i = 0; i < 8; i++){
+                    B[n_solution][i] = A[i];
+                }
+                n_solution++;
+            
+                col = col - 2;
+                row = A[col] + 1;
+                A[7] = 16;
+                A[6] = 16;
+            }
 		}
 
-		if ((col == 0) && (row == 8))
-		{
-			end = 1;
-		}
+		if ((col == 0) && (row == 8)) end = true; //All possible solutions have been found 
 
-		if (col > 0)
-		{
-			for (i = 0; i < col; i++)
-			{
-				if ((row == A[i]) || (row == A[i] + (col - i)) || (row == A[i] - (col - i)))
-				{
-					flag = 0;
+        threat = false;
+		if (col > 0){
+			for (i = 0; i < col; i++){ //Check if position is threatened
+				if ((row == A[i]) || (row == A[i] + (col - i)) || (row == A[i] - (col - i))){
+					threat = true;
 				}
 			}
 		}
-		else
-		{
-			flag = 1;
-		}
 
-		if (row == 8)
-		{
+		if (row == 8){ //Dead end reached
 			A[col] = 16;
-			col--;
+			col--; //Backtrack to previous column
 			row = A[col];
+		}else if (threat == false){ //Valid position found
+			A[col] = row; //Save the position
+			col++; //Move to next column
+			row = -1; //Reset row variable
 		}
-		else if (flag == 1)
-		{
-			A[col] = row;
-			col++;
-			row = -1;
-		}
-
 		row++;
-	} while (end == 0);
-
-	//Check and remove mirrored solutions
-	for (k = 0; k < n_solution-1; k++){
-        if (B[k][0] != 16){
-            for (i = 0; i < 8; i++){
-                //Generate all possible mirrored versions of the solution k
-                C[0][i] = B[k][i];
-                C[1][7 - B[k][i]] = i;
-                C[2][7 - i] = 7 - B[k][i];
-                C[3][B[k][i]] = 7 - i;
-                C[4][7 - i] = B[k][i];
-                C[5][i] = 7 - B[k][i];
-                C[6][7 - B[k][i]] = 7 - i;
-                C[7][B[k][i]] = i;
-            }
-            //Compare all 8 mirrored transformations with all the other solutions
-            //and find if the solutions are the same
-            for (l = n_solution-1; l > k; l--){
-                for (m = 1; m < 8; m++){
-                    mirrored = 1; //Assume that the solutions are the same
-                    for (i = 0; i < 8; i++){
-                        //If one position is different then the
-                        //two solutions are different
-                        if (B[l][i] != C[m][i]){
-                            mirrored = 0;
-                            break;
-                        }
-                    }
-                    if (mirrored == 1){
-                        for (i = 0; i < 8; i++){
-                            //remove solution
-                            B[l][i] = 16;
-                        }
-                    }
-                }
-            }
-        }
-	}
+	} while (end == false);
 
 	// Print solution on stdout
 	for (i = 0; i < n_solution; i++){
-		if (B[i][0] != 16){
-			for (j = 0; j < 8; j++){
-				printf("[%d] ", B[i][j]);
-				if (j == 7){
-					printf("\n");
-				}
-			}
-		}
+        for (j = 0; j < 8; j++){
+            printf("[%d] ", B[i][j]);
+        }
+        printf("\n");
 	}
+
+    //Free arrays
+    for (int i=0; i<n_solution; ++i) {
+        free(B[i]);
+    }
+    free(B);
 
 	return 0;
 }
